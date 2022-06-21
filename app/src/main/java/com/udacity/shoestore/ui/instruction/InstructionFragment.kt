f3283e12738f1bf92e8f3af4b90d37e429c9cddd
lucasnpc <lucasnpc2@outlook.com>
2022-06-21 14:46:19 -0300

    Created Instructions Layout

--- app/src/main/java/com/udacity/shoestore/ui/instruction/InstructionFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/instruction/InstructionFragment.kt
@@ -1,9 +1,32 @@
 package com.udacity.shoestore.ui.instruction
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import dagger.hilt.android.AndroidEntryPoint
+import androidx.navigation.fragment.findNavController
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.InstructionsLayoutBinding
 
-@AndroidEntryPoint
 class InstructionFragment : Fragment() {
+
+    lateinit var binding: InstructionsLayoutBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = InstructionsLayoutBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.instructionsButton.setOnClickListener {
+            findNavController().navigate(R.id.action_instructionFragment_to_shoesListFragment)
+        }
+    }
 
 }
--- app/src/main/java/com/udacity/shoestore/ui/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/welcome/WelcomeFragment.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
+import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.WelcomeLayoutBinding
 
 class WelcomeFragment : Fragment() {
@@ -18,5 +21,12 @@ class WelcomeFragment : Fragment() {
     ): View {
         binding = WelcomeLayoutBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.welcomeButton.setOnClickListener {
+            findNavController().navigate(R.id.action_welcomeFragment_to_instructionFragment)
+        }
     }
 }
